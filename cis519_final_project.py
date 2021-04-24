b4321cc935634cc020d50fdd1c1c480102ace52e
Kachik Ashkaryan <51209942+kashkaryan@users.noreply.github.com>
2021-04-24 12:40:45 -0700

    Update cis519_final_project.py

--- cis519_final_project.py
+++ cis519_final_project.py
@@ -109,7 +109,7 @@ plt.show()
 
 cv_df.groupby('model_name').accuracy.mean()
 
-X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.20, random_state=42)
+X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.30, random_state=42)
 model = LogisticRegression(random_state=0)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
@@ -117,7 +117,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 
-X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.20, random_state=42)
+X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.30, random_state=42)
 tree = RandomForestClassifier(n_estimators=200, random_state=0)
 tree.fit(X_train, y_train)
 y_pred = tree.predict(X_test)
@@ -126,7 +126,7 @@ print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 
 from sklearn.svm import SVC
-X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.20, random_state=42)
+X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.30, random_state=42)
 svclassifier = SVC(C=0.9, kernel='linear', gamma='scale')
 svclassifier.fit(X_train, y_train)
 y_pred = svclassifier.predict(X_test)
